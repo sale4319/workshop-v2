1fe7b4f2110c9c62e6dd0f2beb7f2f2cfadb191b
Aleksandar Stojanovic <sale4319@gmail.com>
2024-09-02 21:17:48 +0200

    feat: add useEffect example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import { About } from "./About";
 import { Homepage } from "./Homepage";
+import { ToastContainer } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </header>
+        <ToastContainer autoClose={1000} hideProgressBar theme="dark" />
+      </div>
+    </>
   );
 }
 
--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Bounce, toast } from "react-toastify";
 import { Favorite } from "./Favorite";
-import { useState } from "react";
 import logo from "./logo.svg";
 
 export const Homepage = () => {
   const navigate = useNavigate();
   const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isLiked) {
+      toast("Nice 🫀", {
+        closeOnClick: true,
+        transition: Bounce,
+      });
+    } else {
+      toast("Why no like? 💔", {
+        closeOnClick: true,
+        transition: Bounce,
+      });
+    }
+  }, [isLiked]);
 
   const handleLike = () => {
     setIsLiked(!isLiked);
