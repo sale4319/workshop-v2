b9d41b2d3aa279527cbd14a58718bf7b7d704b31
Aleksandar Stojanovic <sale4319@gmail.com>
2024-09-02 21:44:14 +0200

    fix: downgrade gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
